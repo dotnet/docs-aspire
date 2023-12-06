808cdfa745de1bbb9d8e3f840c49c21f6610ee71
Alex Wolf <alexwolf@microsoft.com>
2023-12-06 13:57:26 -0500

    updates

--- docs/database/snippets/tutorial/aspiresqlefcore/AspireSQLEFCore/SupportTicket.cs
+++ docs/database/snippets/tutorial/aspiresqlefcore/AspireSQLEFCore/SupportTicket.cs
@@ -1,8 +1,12 @@
-﻿namespace AspireSQLEFCore;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AspireSQLEFCore;
 
 public sealed class SupportTicket
 {
     public int Id { get; set; }
+    [Required]
     public string Title { get; set; } = string.Empty;
+    [Required]
     public string Description { get; set; } = string.Empty;
 }
--- docs/database/snippets/tutorial/aspiresqlefcore/AspireSQLEFCore/TicketContext.cs
+++ docs/database/snippets/tutorial/aspiresqlefcore/AspireSQLEFCore/TicketContext.cs
@@ -5,5 +5,5 @@ namespace AspireSQLEFCore;
 
 public class TicketContext(DbContextOptions options) : DbContext(options)
 {
-    public DbSet<SupportTicket> Tickets => Set<SupportTicket>():
+    public DbSet<SupportTicket> Tickets => Set<SupportTicket>();
 }
