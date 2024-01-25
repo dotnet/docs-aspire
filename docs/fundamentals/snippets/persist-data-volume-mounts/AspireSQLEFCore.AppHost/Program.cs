bf99a527158f8f46b55c1d3a709131f9af6f6651
Alex Wolf <alexwolf@microsoft.com>
2024-01-25 13:02:50 -0500

    tweaks

--- docs/fundamentals/snippets/persist-data-volume-mounts/AspireSQLEFCore.AppHost/Program.cs
+++ docs/fundamentals/snippets/persist-data-volume-mounts/AspireSQLEFCore.AppHost/Program.cs
@@ -8,15 +8,15 @@ var sql = builder.AddSqlServerContainer("sql", password)
     .WithVolumeMount("VolumeMount.sqlserver.data", "/var/opt/mssql", VolumeMountType.Named)
     .AddDatabase("sqldata");
 
-// Persist a PostgreSQL database to a named volume mount
+// Persist a PostgreSQL database to a named volume
 var postgres = builder.AddPostgresContainer("pg", password: password)
     .WithVolumeMount("VolumeMount.postgres.data", "/var/lib/postgresql/data", VolumeMountType.Named)
     .AddDatabase("postgresdb");
 
-// Persist Azure Storage data to a named volume mount
+// Persist Azure Storage data to a named volume
 var storage = builder.AddAzureStorage("Storage")
     .UseEmulator()
-    .WithAnnotation(new VolumeMountAnnotation("VolumeMount.azurite.data", "/data", VolumeMountType.Named)); ;
+    .WithAnnotation(new VolumeMountAnnotation("VolumeMount.azurite.data", "/data", VolumeMountType.Named));
 
 var blobs = storage.AddBlobs("BlobConnection");
 
