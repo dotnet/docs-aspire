9927cb0f1459eb6dd5f8e260987aaafa621742b4
Alex Wolf <alexwolf@microsoft.com>
2024-01-25 12:51:44 -0500

    volume mount

---
+++ docs/fundamentals/snippets/persist-data-volume-mounts/AspireSQLEFCore.AppHost/Program.cs
@@ -0,0 +1,28 @@
+﻿var builder = DistributedApplication.CreateBuilder(args);
+
+// Retrieve the password you created from user secrets
+var password = builder.Configuration["samplepassword"];
+
+// Persist a SQL Server database to a named volume mount
+var sql = builder.AddSqlServerContainer("sql", password)
+    .WithVolumeMount("VolumeMount.sqlserver.data", "/var/opt/mssql", VolumeMountType.Named)
+    .AddDatabase("sqldata");
+
+// Persist a PostgreSQL database to a named volume mount
+var postgres = builder.AddPostgresContainer("pg", password: password)
+    .WithVolumeMount("VolumeMount.postgres.data", "/var/lib/postgresql/data", VolumeMountType.Named)
+    .AddDatabase("postgresdb");
+
+// Persist Azure Storage data to a named volume mount
+var storage = builder.AddAzureStorage("Storage")
+    .UseEmulator()
+    .WithAnnotation(new VolumeMountAnnotation("VolumeMount.azurite.data", "/data", VolumeMountType.Named)); ;
+
+var blobs = storage.AddBlobs("BlobConnection");
+
+builder.AddProject<Projects.AspireSQLEFCore>("aspiresql")
+    .WithReference(sql)
+    .WithReference(postgres)
+    .WithReference(blobs);
+
+builder.Build().Run();
---
+++ docs/fundamentals/snippets/persist-data-volume-mounts/AspireSQLEFCore/SupportTicket.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AspireSQLEFCore;
+
+public sealed class SupportTicket
+{
+    public int Id { get; set; }
+    [Required]
+    public string Title { get; set; } = string.Empty;
+    [Required]
+    public string Description { get; set; } = default!;
+    [Required]
+    [NotMapped]
+    public IFormFile Document { get; set; } = default!;
+}
