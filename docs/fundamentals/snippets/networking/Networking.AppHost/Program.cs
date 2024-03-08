908216ead71374d3a223e8a8eb1d459113f592e9
David Pine <david.pine.7@gmail.com>
2024-03-08 11:17:45 -0600

    Add content covering `WithEndpoint` and the `callback` delegate. (#472)
    * Fixes #426
    
    * Add service defaults and API
    
    * Got the WithEndpoint + callback working
    
    * Set transport to http
    
    * Correct description

--- docs/fundamentals/snippets/networking/Networking.AppHost/Program.cs
+++ docs/fundamentals/snippets/networking/Networking.AppHost/Program.cs
@@ -1,5 +1,17 @@
 ﻿var builder = DistributedApplication.CreateBuilder(args);
 
+var apiService = builder.AddProject<Projects.Networking_ApiService>("apiService")
+    .WithEndpoint("admin", static endpoint =>
+    {
+        endpoint.Port = 17003;
+        endpoint.UriScheme = "http";
+        endpoint.Transport = "http";
+    });
+
+builder.AddProject<Projects.Networking_Frontend>("frontend")
+       .WithReference(apiService);
+
+// WithEndpoint(builder);
 // ContainerPort(builder);
 // EnvVarPort(builder);
 // HostPortWithRandomServicePort(builder);
