4421eafe8cb22b22900aa6055afb0c74b959545b
David Pine <david.pine@microsoft.com>
2024-04-26 12:42:39 -0500

    Local snippets, and updates to address #672

---
+++ docs/fundamentals/snippets/volumes/VolumeMounts.AppHost/Program.WithBindMount.cs
@@ -0,0 +1,13 @@
+﻿internal partial class Program
+{
+    private static IResourceBuilder<SqlServerDatabaseResource> WithDataMount(IDistributedApplicationBuilder builder)
+    {
+        // <with-data-mount>
+        var sql = builder.AddSqlServer("sql")
+                         .WithBindMount("VolumeMount.AppHost-sql-data", "/var/opt/mssql")
+                         .AddDatabase("sqldb");
+        // </with-data-mount>
+
+        return sql;
+    }
+}
