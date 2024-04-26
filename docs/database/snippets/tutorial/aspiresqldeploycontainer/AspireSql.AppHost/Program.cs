864c37147f4af0a581dec9acd36d6ab1bb15d081
alexwolfmsft <93200798+alexwolfmsft@users.noreply.github.com>
2024-04-26 13:28:03 -0400

    Updates and fixes for SQL deployment (#758)
    * updates and fixes
    
    * fix
    
    * verbiage
    
    * removed files
    
    * verbiage
    
    * missing line

---
+++ docs/database/snippets/tutorial/aspiresqldeploycontainer/AspireSql.AppHost/Program.cs
@@ -0,0 +1,13 @@
+﻿var builder = DistributedApplication.CreateBuilder(args);
+
+var apiService = builder.AddProject<Projects.AspireSql_ApiService>("apiservice");
+
+// Provisions a containerized SQL Server database when published
+var sqlServer2 = builder.AddSqlServer("sqlserver")
+                      .AddDatabase("sqldb");
+
+builder.AddProject<Projects.AspireSql_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    .WithReference(apiService);
+
+builder.Build().Run();
