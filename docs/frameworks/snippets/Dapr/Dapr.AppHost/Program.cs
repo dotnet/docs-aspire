eef29ae33052db4dcc830722ff4afe0c50b77d2c
David Pine <david.pine@microsoft.com>
2024-05-17 13:11:15 -0500

    [DRAFT] Dapr content (#894)
    * Initial bits
    
    * Intro to the Dapr/Aspire doc (#902)
    
    * add draft intro
    
    Signed-off-by: Hannah Hunter <hannahhunter@microsoft.com>
    
    * intro tweaks
    
    Signed-off-by: Hannah Hunter <hannahhunter@microsoft.com>
    
    * edits to the intro
    
    Signed-off-by: Hannah Hunter <hannahhunter@microsoft.com>
    
    * Apply suggestions from code review
    
    ---------
    
    Signed-off-by: Hannah Hunter <hannahhunter@microsoft.com>
    Co-authored-by: David Pine <david.pine@microsoft.com>
    
    * Dapr bits (#929)
    
    * adds .net aspire & dapr documentation
    
    * add dapr init instructions
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    * Update docs/frameworks/dapr.md
    
    * Update docs/frameworks/dapr.md
    
    * Update dapr.md
    
    * Update dapr.md
    
    ---------
    
    Co-authored-by: Nico Vermeir <136470992+NVermeir@users.noreply.github.com>
    Co-authored-by: David Pine <david.pine@microsoft.com>
    
    * Apply suggestions from code review
    
    ---------
    
    Signed-off-by: Hannah Hunter <hannahhunter@microsoft.com>
    Co-authored-by: Hannah Hunter <94493363+hhunter-ms@users.noreply.github.com>
    Co-authored-by: NicoVermeir <nico_vermeir@hotmail.com>
    Co-authored-by: Nico Vermeir <136470992+NVermeir@users.noreply.github.com>

---
+++ docs/frameworks/snippets/Dapr/Dapr.AppHost/Program.cs
@@ -0,0 +1,23 @@
+﻿using Aspire.Hosting.Dapr;
+
+var builder = DistributedApplication.CreateBuilder(args);
+builder.AddDapr();
+
+DaprSidecarOptions sidecarOptions = new ()
+{
+    AppId = "apiservice-dapr",
+    DaprGrpcPort = 50001,
+    DaprHttpPort = 3500,
+    MetricsPort = 9090
+};
+
+var apiService = builder
+    .AddProject<Projects.Dapr_ApiService>("apiservice")
+    .WithDaprSidecar(sidecarOptions);
+
+builder.AddProject<Projects.Dapr_Web>("webfrontend")
+    .WithExternalHttpEndpoints()
+    .WithReference(apiService)
+    .WithDaprSidecar("webfrontend-dapr");
+
+builder.Build().Run();
