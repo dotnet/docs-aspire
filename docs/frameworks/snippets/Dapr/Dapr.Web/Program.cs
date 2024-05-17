08354318d106a406bece0417ba15cf3d81fca638
Nico Vermeir <136470992+NVermeir@users.noreply.github.com>
2024-05-17 15:20:44 +0200

    adds .net aspire & dapr documentation

---
+++ docs/frameworks/snippets/Dapr/Dapr.Web/Program.cs
@@ -0,0 +1,40 @@
+﻿using Dapr.Web;
+using Dapr.Web.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add service defaults & Aspire components.
+builder.AddServiceDefaults();
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddOutputCache();
+
+builder.Services.AddDaprClient();
+
+builder.Services.AddTransient<WeatherApiClient>();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.UseOutputCache();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.MapDefaultEndpoints();
+
+app.Run();
