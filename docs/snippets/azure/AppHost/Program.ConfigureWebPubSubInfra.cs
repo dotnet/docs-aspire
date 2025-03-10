4d762cc71411a7dfd573c05ef480ac2fb8ae14d4
David Pine <david.pine@microsoft.com>
2025-03-10 14:57:15 -0500

    Fixes #2317

---
+++ docs/snippets/azure/AppHost/Program.ConfigureWebPubSubInfra.cs
@@ -0,0 +1,21 @@
+﻿using Azure.Provisioning.WebPubSub;
+
+internal static partial class Program
+{
+    public static void ConfigureWebPubSubInfra(IDistributedApplicationBuilder builder)
+    {
+        // <configure>
+        builder.AddAzureWebPubSub("web-pubsub")
+            .ConfigureInfrastructure(infra =>
+            {
+                var webPubSubService = infra.GetProvisionableResources()
+                                            .OfType<WebPubSubService>()
+                                            .Single();
+
+                webPubSubService.Sku.Name = "Standard_S5";
+                webPubSubService.Sku.Capacity = 5;
+                webPubSubService.Tags.Add("ExampleKey", "Example value");
+            });
+        // </configure>
+    }
+}
