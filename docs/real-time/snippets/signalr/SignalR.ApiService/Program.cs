8d9abb85c2e7eb1d728f27379879fea639203e7b
David Pine <david.pine@microsoft.com>
2025-03-26 09:33:17 -0500

    Azure SignalR Service content rewrite (#2775)
    * Initial bits
    
    * Draft - WIP.
    
    * More updates
    
    * I think this is accurate
    
    * This still isn't working
    
    * Remove logging, tracing, and metrics
    
    * More updates
    
    * Clarifying text
    
    * Address more feedback in SignalR content
    
    * Apply suggestions from code review
    
    Co-authored-by: Andy (Steve) De George <67293991+adegeo@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Andy (Steve) De George <67293991+adegeo@users.noreply.github.com>

--- docs/real-time/snippets/signalr/SignalR.ApiService/Program.cs
+++ docs/real-time/snippets/signalr/SignalR.ApiService/Program.cs
@@ -2,16 +2,77 @@
 
 builder.AddServiceDefaults();
 
+builder.Services.AddMemoryCache();
+builder.Services.AddSingleton<ServiceHubContextFactory>();
 builder.Services.AddProblemDetails();
+builder.Services.AddOpenApi();
 
-builder.Services.AddSignalR()
-                .AddNamedAzureSignalR("signalr");
+var isServerlessMode = builder.Configuration.GetValue<bool>("IS_SERVERLESS");
 
+if (!isServerlessMode)
+{
+    builder.Services.AddSignalR()
+                    .AddNamedAzureSignalR("signalr");
+}
+else
+{
+    builder.Services.AddSingleton(sp =>
+    {
+        return new ServiceManagerBuilder()
+            .WithOptions(options =>
+            {
+                options.ConnectionString = builder.Configuration.GetConnectionString("signalr");
+            })
+            .WithLoggerFactory(sp.GetRequiredService<ILoggerFactory>())
+            .BuildServiceManager();
+    });
+}
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference(_ => _.Servers = []);
+}
 
 app.UseExceptionHandler();
 
-app.MapHub<ChatHub>(HubEndpoints.ChatHub);
+var options = new JsonSerializerOptions(JsonSerializerDefaults.Web)
+{
+    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+};
+
+if (isServerlessMode)
+{
+    app.MapPost("/chathub/negotiate", async (string? userId, ServiceHubContextFactory factory) =>
+    {
+        var hubContext = await factory.GetOrCreateHubContextAsync("chathub", CancellationToken.None);
+
+        NegotiationResponse negotiateResponse = await hubContext.NegotiateAsync(new()
+        {
+            UserId = userId ?? "user-1",
+        });
+
+        return Results.Json(negotiateResponse, new JsonSerializerOptions(JsonSerializerDefaults.Web)
+        {
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+        });
+    });
+
+    // try in the command line `CURL -X POST https://localhost:53282/broadcast` to broadcast messages to the clients
+    app.MapPost("/chathub/broadcast", async (ServiceHubContextFactory factory) =>
+    {
+        var hubContext = await factory.GetOrCreateHubContextAsync("chathub", CancellationToken.None);
+
+        await hubContext.Clients.All.SendAsync(
+            HubEventNames.MessageReceived,
+            new UserMessage("server", "Started..."));
+    });
+}
+else
+{
+    app.MapHub<ChatHub>(HubEndpoints.ChatHub);
+}
 
 app.MapDefaultEndpoints();
 
