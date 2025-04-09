3cbbfb34ddcd7ae4e74b7b3cbf3cd1b530a29fa3
David Pine <david.pine@microsoft.com>
2025-04-09 10:15:13 -0500

    Add content on customizing resource URLs (#2940)
    * Fixes #2936
    
    * Correct TOC
    
    * Minor tweaks
    
    * Apply suggestions from code review
    
    Co-authored-by: Genevieve Warren <24882762+gewarren@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Damian Edwards <damian@damianedwards.com>
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Genevieve Warren <24882762+gewarren@users.noreply.github.com>
    Co-authored-by: Damian Edwards <damian@damianedwards.com>

---
+++ docs/fundamentals/snippets/custom-urls/AspireApp.AppHost/Program.WithUrl.cs
@@ -0,0 +1,15 @@
+﻿internal static partial class Program
+{
+    internal static void WithUrlExample(string[] args)
+    {
+        // <withurl>
+        var builder = DistributedApplication.CreateBuilder(args);
+
+        var api = builder.AddProject<Projects.AspireApp_Api>("api");
+
+        api.WithUrl($"{api.GetEndpoint("https")}/admin"), "Admin Portal");
+
+        builder.Build().Run();
+        // </withurl>
+    }
+}
---
+++ docs/fundamentals/snippets/custom-urls/AspireApp.AppHost/Program.WithUrls.cs
@@ -0,0 +1,29 @@
+﻿internal static partial class Program
+{
+    internal static void WithUrlsExample(string[] args)
+    {
+        // <withurls>
+        var builder = DistributedApplication.CreateBuilder(args);
+
+        builder.AddProject<Projects.AspireApp_Api>("api")
+            .WithUrls(context =>
+            {
+                foreach (var tuple in context.Urls
+                    .Select(url => (Url: url, Uri: new Uri(url.Url)))
+                    .OrderByDescending(_ => _.Uri.Scheme is "https")
+                    .Select((pair, index) => (pair.Url, pair.Uri.Scheme, Index: index)))
+                {
+                    var (url, scheme, index) = tuple;
+
+                    // Order HTTPS first.
+                    var order = context.Urls.Count - 1 - index;
+
+                    url.DisplayText = $"{index + 1}. {scheme.ToUpper()}";
+                    url.DisplayOrder = order;
+                }
+            });
+
+        builder.Build().Run();
+        // </withurls>
+    }
+}
